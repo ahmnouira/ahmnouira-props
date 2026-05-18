b29485180e2a7e0b31f299610dfd6cb6a20cb52d
Ahmed Nouira <ahmnouira@gmail.com>
2026-05-18 07:24:03 +0100

    fix: fixed typos

--- src/props/nav-item-props.ts
+++ src/props/nav-item-props.ts
@@ -1,4 +1,6 @@
-export type NaItemProps<T = string> = {
+import { SelectProps } from "./select-props";
+
+export type NavItemProps<T = string> = SelectProps<T> & {
   href?: string;
   title?: T;
   pathname?: string;
